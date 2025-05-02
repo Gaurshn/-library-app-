1ca352d2d8fcc01112e33282db79daaf92ea03c0
Gauravshn <gauravsahani36@gmail.com>
2025-05-02 16:05:45 +0530

    changed name from test branch

--- front-end/aayansh-library-frontend/src/App.js
+++ front-end/aayansh-library-frontend/src/App.js
@@ -10,7 +10,7 @@ import LogIn from "./components/loginpage";
 
 const ownerDetail = [
   {
-    name: "Aditya",
+    name: "Aditya sahanigit",
     designation: "Owner",
     phoneno: 7457021151,
     emailid: "adityaSahani36@gmail.com",
